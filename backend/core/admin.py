67b399bb565c32150e9895df5173f1f2d3cedeb2
Demyd Siedykh <demid.sedyh@nixsolutions.com>
2021-06-10 11:08:51 +0300

    Add appropriate structure to polls

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 
-from core.models import Poll
+from core.models import Poll, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
 
 
 @admin.register(Poll)
 class TopicAdmin(admin.ModelAdmin):
-
+    inlines = [
+        ChoiceInline,
+    ]
     list_display = ('question', 'created_at')
 
     @staticmethod
     def preview(obj):
         return obj.question[:40]
+
+
