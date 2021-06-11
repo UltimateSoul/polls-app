885d59b09756b54e85564cf0be81dbdd9dc1701e
Demyd Siedykh <demid.sedyh@nixsolutions.com>
2021-06-11 12:35:34 +0300

    Resolve many issues with swager and vote action args

--- backend/api/views.py
+++ backend/api/views.py
@@ -26,15 +26,21 @@ class PollViewSet(viewsets.ModelViewSet):
 
     @swagger_auto_schema(description="Voting in polls", request_body=ResultSerializer,
                          response={status.HTTP_201_CREATED: ResultSerializer})
-    @action(methods=['POST'], detail=False, url_name='vote', url_path='vote')
+    @action(methods=['POST'], serializer_class=ResultSerializer, detail=False, url_name='vote', url_path='vote')
     def vote(self, request, *args, **kwargs):  # noqa
         serializer = ResultSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         ip_address = get_client_ip(self.request)
-        ip_address_exists = serializer.validated_data.get('poll').results.filter(ip_address=ip_address)
+        poll = serializer.validated_data.get('poll')
+        choice = serializer.validated_data.get('choice')
+        ip_address_exists = poll.results.filter(ip_address=ip_address)
         if ip_address_exists:
             data = {"detail": "You have already voted!"}
             return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response({'Message': 'You\'ve voted successfully!'}, status=status.HTTP_201_CREATED, headers=headers)
+        choice_connected_to_poll = poll.choices.filter(id=choice.id).exists()
+        if choice_connected_to_poll:
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response({'Message': 'You\'ve voted successfully!'}, status=status.HTTP_201_CREATED, headers=headers)
+        data = {"detail": 'This choice doesn\'t connected to poll itself!'}
+        return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
